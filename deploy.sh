88a9bcc466861d720b3b8d668a6556315489e267
leoplouvier <plouvierleo@gmail.com>
2017-01-20 10:46:55 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 
 #!/bin/bash
-ssh root@46.101.224.186
+tail -n +3 "S0" | ssh root@$46.101.224.186 ; exit
 set -eu
 
 apt-get update
 apt-get upgrade
-apt-get install nginx
+apt-get intall -y ssh
+apt-get install -y nginx
+
 
