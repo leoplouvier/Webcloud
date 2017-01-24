8c4d9d26132f5d54c052e01809d532865eea694b
leoplouvier <plouvierleo@gmail.com>
2017-01-24 12:35:08 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
 
-#!/bin/bash
+# !/bin/bash
+#connection 
 tail -n +3 "S0" | ssh root@46.101.224.186 ; exit
+
+#Exclusion des erreurs
 set -eu
 
+#Mise à jour de la VM
 apt-get update
 apt-get upgrade
-apt-get intall -y ssh
-apt-get install -y nginx
 
-ps>/var/mm/html/index.html
+#installation du serveur web et de l'outil git
+apt-get install nginx -y
+apt-get install git -y
+
+#placement dans le répertoire /var/www/html/
+cd /var/www/html/
+
+#importation du projet github
+git clone https://github.com/leoplouvier/Webcloud
