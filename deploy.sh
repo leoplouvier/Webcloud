d0787c1aebbf51ef750d910185b5fd85009b5792
leoplouvier <plouvierleo@gmail.com>
2017-01-26 20:08:56 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
-
 # !/bin/bash
-#connection 
-tail -n +3 "S0" | ssh root@46.101.224.186 ; exit
 
 #Exclusion des erreurs
 set -eu
@@ -27,3 +24,24 @@ cp index.html Webcloud
 cd Webcloud 
 chmod +rwx deploy.sh
 chmod +rwx index.html
+
+### Pour Mettre un wikipédia local (kiwix) : 
+
+# Téléchargement de kiwix
+wget http://download.kiwix.org/bin/kiwix-linux-x86_64.tar.bz2
+
+#dézipper le fichier
+bunzip2 kiwix-linux-x86_64.tar.bz2
+tar xvf kiwix-linux-x86_64.tar
+
+#Télécharger le fichier .zim (version francaise sans les images ici) 
+#--- pour la version complète remplacer wikipedia_fr_all_nopic_2016-05.zim par wikipedia_fr_all_2016-06.zim 
+wget https://download.kiwix.org/zim/wikipedia/wikipedia_fr_all_nopic_2016-05.zim
+
+#Démarrer kiwix-serve
+./kiwix/bin/kiwix-serve --port=8080 wikipedia_fr_all_nopic_2016-05.zim; exit
+
+# Une fois kiwix-serve lancé 
+# vous pouvez accédez au wikipédia local depuis votre navigateur web 
+# en recherchant l'adresse http://xxx.xxx.xxx.xxx:80/ 
+# xxx.xxx.xxx.xxx étant l'adresse IP de votre serveur
